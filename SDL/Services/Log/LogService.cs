826a90343e84967cdeaa592eb1766cd5aad7c71d
Odair José Gambert Nossa <odairjgn@outlook.com>
2024-04-23 18:04:43 -0300

    Fix

---
+++ SDL/Services/Log/LogService.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+namespace SDL.Services.Log
+{
+    public class LogService
+    {
+        private static LogService? instance;
+        public static LogService Instance => instance ??= new LogService();
+
+        private LogService() { }
+
+        public void WriteException(Exception ex)
+        {
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine(ex.ToString());
+            ResetColor();
+        }
+    }
+}
