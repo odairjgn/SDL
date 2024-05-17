1f3feebc9a96db47c44d8c2833d16b067bb05519
odairjgn <odairjgn@outlook.com>
2024-05-16 21:34:26 -0300

    Download metadata

--- SDL/Services/Tasks/TrackDownloadTask.cs
+++ SDL/Services/Tasks/TrackDownloadTask.cs
@@ -1,9 +1,14 @@
 ﻿using SDL.Models.Enum;
+using SDL.Services.Configuration;
 using SDL.Services.Ffmpeg;
 using SDL.Services.Log;
 using SDL.Services.Utils;
+using SDL.SpotifyClient.Models.Album;
 using SDL.SpotifyClient.Models.Tracks;
 using SDL.SpotifyClient.Services;
+using System.Configuration;
+using System.IO;
+using System.Text.RegularExpressions;
 using YoutubeExplode;
 using YoutubeExplode.Videos.Streams;
 
@@ -74,6 +79,13 @@ namespace SDL.Services.Tasks
                 var streamUrl = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate().Url;
                 SetupDirectory();
                 var ok = await FfmpegService.Instance.StreamConvert(streamUrl, _file.FullName);
+
+                if (ok)
+                {
+                    await AddMetadataAsync();
+                    await AddCoverAsync();
+                }
+
                 Status = ok ? DownloadTaskStatus.Finished : DownloadTaskStatus.Error;
             }
             catch (Exception ex)
@@ -82,6 +94,70 @@ namespace SDL.Services.Tasks
                     _file.Delete();
 
                 Status = DownloadTaskStatus.Error;
+                LogService.Instance.WriteException(ex);
+            }
+        }
+
+        private async Task AddMetadataAsync()
+        {
+            try
+            {
+                if (!ConfigurationService.ConfigFile.CreateSongMetadata)
+                    return;
+
+
+                var file = TagLib.File.Create(_file.FullName);
+                file.Tag.Title = _track.Title;
+                file.Tag.Performers = _track.Artists.Select(x => x.Name).ToArray();
+                file.Tag.Album = _track.Album?.Name??string.Empty;
+                file.Tag.Track = (uint)_track.TrackNumber;
+                file.Tag.DateTagged = DateTime.UtcNow;
+                file.Tag.Disc = (uint)_track.DiscNumber;
+                file.Tag.Genres = _track.Album?.Genres?.ToArray() ?? Array.Empty<string>();
+                file.Save();
+            }
+            catch (Exception ex)
+            {
+                LogService.Instance.WriteException(ex);
+            }
+        }
+
+        private async Task AddCoverAsync()
+        {
+            try
+            {
+                if (!ConfigurationService.ConfigFile.DownloadCovers)
+                    return;
+
+                var url = _track.Album?.Images?.FirstOrDefault()?.Url;
+
+                if (string.IsNullOrEmpty(url))
+                    return;
+
+                var clientHttp = new HttpClient();
+                var imgdata = await clientHttp.GetByteArrayAsync(url);
+
+                TagLib.Picture pic = new TagLib.Picture
+                {
+                    Type = TagLib.PictureType.FrontCover,
+                    Description = "Cover",
+                    MimeType = System.Net.Mime.MediaTypeNames.Image.Jpeg
+                };
+
+                var file = TagLib.File.Create(_file.FullName);
+                using var ms = new MemoryStream();
+                using var buffer = new MemoryStream(imgdata);
+                using var image = Image.FromStream(buffer);
+                image.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                ms.Position = 0;
+                pic.Data = TagLib.ByteVector.FromStream(ms);
+                file.Tag.Pictures = [pic];
+                file.Save();
+                ms.Close();
+                buffer.Close();
+            }
+            catch (Exception ex)
+            {
                 LogService.Instance.WriteException(ex);
             }
         }
